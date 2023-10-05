5683b8a3bfc0aac38da2c98cbd6d51b05e33f43d
Aaronc-r <145399612+Aaronc-r@users.noreply.github.com>
2023-10-04 20:14:30 -0700

    created IsCaptOnMap

---
+++ IsCaptOnMap.m
@@ -0,0 +1,9 @@
+function mapCheck = IsCaptOnMap (x,y,width,height)
+
+if(x>0 && x<width && y>0 && y<height)
+  mapCheck=1;
+else
+  mapCheck=0;
+endif
+
+endfunction
--- crabs.m
+++ crabs.m
@@ -18,19 +18,21 @@ sizeCapt = 50;
 captGraphics = drawCaptain(xCapt, yCapt, thetaCapt, sizeCapt)
 
 % initial command
-cmd = "null"; 
+cmd = "null"; % initial command
 
-while ( cmd != "Q") 
-  
-cmd = kbhit(); % Read the keyboard.
+while ( cmd != "Q") % While not quit, read keyboard and respond
 
-if( cmd == "w" || cmd == "a" || cmd == "d" ) %Captain has moved. Respond.
+cmd = kbhit(); % Read the keyboard .
+
+if( cmd == "w" || cmd == "a" || cmd == "d" ) % Captain has moved . Respond .
+
 % erase old captain
 for i=1:length( captGraphics )
 set( captGraphics(i), 'Visible', 'off' );
 endfor
+
 % move capt
-[xCapt, yCapt, thetaCapt] = moveCapt(cmd, xCapt, yCapt, thetaCapt);
+[xCapt, yCapt, thetaCapt] = moveCapt(cmd, xCapt, yCapt, thetaCapt, mapWidth, mapHeight);
 
 % draw new capt
 captGraphics = drawCaptain( xCapt, yCapt, thetaCapt, sizeCapt);
