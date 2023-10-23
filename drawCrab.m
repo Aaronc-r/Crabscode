30adec0b15774fb0c748e261e967108faf524665
Aaronc-r <145399612+Aaronc-r@users.noreply.github.com>
2023-10-22 17:22:08 -0700

    updated the crab

--- drawCrab.m
+++ drawCrab.m
@@ -1,36 +1,72 @@
 function crabGraphics = drawCrab (xCrab,yCrab,thetaCrab,sizeCrab)
-% This function draws the crab by rotating and shifting the crab
-% as it appears in getCrab.m where it is at the origin with zero heading.
-%get crab at the origin
-crab = getCrab(sizeCrab);
-%rotate crab
-R = getRotation(thetaCrab);
-crabRotated = R*crab;
-%shift crab to new location
-S = getTranslation(xCrab,yCrab);
-crab = S*crabRotated;
-%get crab points
-pt1=crab(:,1);
-pt2=crab(:,2);
-pt3=crab(:,3);
-pt4=crab(:,4);
-pt5=crab(:,5);
-pt6=crab(:,6);
-pt7=crab(:,7);
-pt8=crab(:,8);
-pt9=crab(:,9);
-pt10=crab(:,10);
-%draw the crab body
-crabGraphics(1) = drawLine(pt1,pt2,"r");
-crabGraphics(2) = drawLine(pt2,pt3,"r");
-crabGraphics(3) = drawLine(pt3,pt4,"r");
-crabGraphics(4) = drawLine(pt4,pt1,"r");
-%draw crab pinchers
-crabGraphics(5) = drawLine(pt1,pt5,"r");
-crabGraphics(6) = drawLine(pt4,pt6,"r");
-% draw legs
-crabGraphics(7) = drawLine(pt2,pt7,"r");
-crabGraphics(8) = drawLine(pt7,pt8,"r");
-crabGraphics(9) = drawLine(pt3,pt9,"r");
-crabGraphics(10) = drawLine(pt9,pt10,"r");
+
+ % This function draws the crab by rotating and shifting the crab
+ % as it appears in getCrab . m where it is at the origin with zero heading .
+
+  %get crab at the origin
+  crab = getCrab(sizeCrab);
+
+  % rotate crab
+  R = getRotation(thetaCrab);
+  crabRotated = R*crab;
+  % shift crab to new location
+  S = getTranslation(xCrab,yCrab);
+  crab = S*crabRotated;
+
+  % get crab points
+   Pt1=crab(:,1);
+   Pt2=crab(:,2);
+   Pt3=crab(:,3);
+   Pt4=crab(:,4);
+   Pt5=crab(:,5);
+   Pt6=crab(:,6);
+   Pt7=crab(:,7);
+   Pt8=crab(:,8);
+   Pt9=crab(:,9);
+   Pt10=crab(:,10);
+   Pt11=crab(:,11);
+   Pt12=crab(:,12);
+   Pt13=crab(:,13);
+   Pt14=crab(:,14);
+   Pt15=crab(:,15);
+   Pt16=crab(:,16);
+   Pt17=crab(:,17);
+   Pt18=crab(:,18);
+   Pt19=crab(:,19);
+   Pt20=crab(:,20);
+
+  % Right Leg
+  crabGraphics(1) = drawLine( Pt1, Pt2,"r");
+
+  % Left Leg
+  crabGraphics(2) = drawLine( Pt3, Pt4,"r");
+
+  % left Body with arm
+  crabGraphics(3) = drawLine( Pt5, Pt6,"r");
+  crabGraphics(4) = drawLine( Pt6, Pt7,"r");
+  crabGraphics(5) = drawLine( Pt7, Pt8,"r");
+
+  % Right body with arm
+  crabGraphics(6) = drawLine( Pt9, Pt10,"r");
+  crabGraphics(7) = drawLine( Pt10, Pt11,"r");
+  crabGraphics(8) = drawLine( Pt11, Pt12,"r");
+
+  % Body upper and lower connections
+  crabGraphics(9) = drawLine( Pt5, Pt9,"r");
+  crabGraphics(10) = drawLine( Pt6, Pt10,"r");
+
+  % Right Eye
+  crabGraphics(11) = drawLine( Pt13, Pt14,"r");
+
+  % Left Eye
+  crabGraphics(12) = drawLine( Pt15, Pt16,"r");
+
+  % Right Claw
+  crabGraphics(13) = drawLine( Pt8, Pt17,"r");
+  crabGraphics(14) = drawLine( Pt8, Pt18,"r");
+
+  % Left Claw
+  crabGraphics(15) = drawLine( Pt12, Pt19,"r");
+  crabGraphics(16) = drawLine( Pt12, Pt20,"r");
+
 endfunction
--- getCrab.m
+++ getCrab.m
@@ -1,19 +1,35 @@
 function crab = getCrab (sizeCrab)
 % This function stores and returns the crab at the origin with zero heading in the
-%form of a matirx.
-% Each colunm represents one point on the crab.
-%body
-crabPt1 = [ sizeCrab/2; sizeCrab; 1];
-crabPt2 = [ -sizeCrab/2; sizeCrab; 1];
-crabPt3 = [ -sizeCrab/2; -sizeCrab; 1];
-crabPt4 = [ sizeCrab/2; -sizeCrab; 1];
-%claws
-crabPt5 = [ 2*sizeCrab; sizeCrab/4; 1];
-crabPt6 = [ 2*sizeCrab; -sizeCrab/4; 1];
-%legs
-crabPt7 = [0; 1.5*sizeCrab; 1];
-crabPt8 = [-sizeCrab; 1.5*sizeCrab; 1];
-crabPt9 = [0; -1.5*sizeCrab; 1];
-crabPt10 = [-sizeCrab; -1.5*sizeCrab; 1];
-crab = [ crabPt1, crabPt2, crabPt3, crabPt4, crabPt5, crabPt6, crabPt7, crabPt8, crabPt9, crabPt10];
+% form of a matirx .
+% Each colunm represents one point on the crab .
+% Right Leg
+crabPt1 = [ -1.25*sizeCrab; 1.5*sizeCrab; 1];
+crabPt2 = [ -.75*sizeCrab; sizeCrab; 1];
+% Left Leg
+crabPt3 = [-1.25*sizeCrab; -1.5*sizeCrab; 1];
+crabPt4 = [ -.75*sizeCrab; -sizeCrab; 1];
+% Left side of body (including arm)
+crabPt5 = [ -sizeCrab; -sizeCrab; 1];
+crabPt6 = [ 0; -sizeCrab; 1];
+crabPt7 = [ .5*sizeCrab; -2*sizeCrab; 1];
+crabPt8 = [ sizeCrab; -.75*sizeCrab; 1];
+% Right side of body (including arm)
+crabPt9 = [ -sizeCrab; sizeCrab; 1];
+crabPt10 = [ 0; sizeCrab; 1];
+crabPt11 = [ .5*sizeCrab; 2*sizeCrab; 1];
+crabPt12 = [ sizeCrab; .75*sizeCrab; 1];
+% left Eye
+crabPt13 = [ 0; -.5*sizeCrab; 1];
+crabPt14 = [ .5*sizeCrab; -.5*sizeCrab; 1];
+% Right Eye
+crabPt15 = [ 0; .5*sizeCrab; 1];
+crabPt16 = [ .5*sizeCrab; .5*sizeCrab; 1];
+% Left Claw
+crabPt17 = [ .75*sizeCrab; -.5*sizeCrab; 1];
+crabPt18 = [ 1.25*sizeCrab; -.5*sizeCrab; 1];
+% Right Claw
+crabPt19 = [ .75*sizeCrab; .5*sizeCrab; 1];
+crabPt20 = [ 1.25*sizeCrab; .5*sizeCrab; 1];
+crab = [ crabPt1, crabPt2, crabPt3, crabPt4, crabPt5, crabPt6, crabPt7, crabPt8, crabPt9, crabPt10,crabPt11, crabPt12, crabPt13,...
+crabPt14, crabPt15, crabPt16, crabPt17, crabPt18, crabPt19, crabPt20];
 endfunction
